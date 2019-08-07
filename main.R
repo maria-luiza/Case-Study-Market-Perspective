3ffca4ef3aaae40f63df1f4a4ad08451ad7c6518
Maria Luiza <mlnr@cesar.org.br>
2019-08-07 12:28:17 -0300

    Adding state evaluation

--- main.R
+++ main.R
@@ -7,6 +7,7 @@ source("analysis.R") # Functions to extract info on data
 source("plotter.R") # Functions to plotter data
 
 library(reshape2)
+require(plyr)
 require(dplyr)
 require(gridExtra)
 
@@ -71,7 +72,7 @@ names(data_gen) <- c("Deal", "Lost")
 # 4. Data Evaluation
 
 # 4.1 Value per month per Year
-png(filename = "value_month_year.png", width = 825, height = 660, units = "px")
+png(filename = "value_month_year.png", width = 1200, height = 600, units = "px", res=100)
 
 plots <- list()
 for(year in years){
@@ -96,7 +97,7 @@ do.call(grid.arrange, c(plots, nrow=length(years)))
 dev.off()
 
 # 4.2 Sales executive thourgh years
-png(filename = "sales_executive_years.png", width = 770, height = 665, units = "px")
+png(filename = "sales_executive_years.png", width = 770, height = 665, units = "px", res=100)
 
 plots <- list()
 for(year in years){
@@ -140,8 +141,22 @@ for(year in years){
 print(clients)
 
 # 4.4 Summary Boxplot
-png(filename = "summary_years.png", width = 825, height = 660, units = "px")
+png(filename = "summary_years.png", width = 825, height = 660, units = "px", res=150)
+
 # Get only information used to evaluate
-box_data <- select(data, Year, Value, Lost_Deal)
-plot_boxplot_summary(box_data)
+data_info <- select(data, Year, State, Value, Lost_Deal)
+plot_boxplot_summary(data_info)
+dev.off()
+
+# # 4.5 State Report
+png(filename = "state_report.png", width = 1600, height = 800, units = "px", res=150)
+data_info <- aggregate(. ~State + Year + Lost_Deal, data=data_info, sum, na.rm=TRUE)
+prop_data <-ddply(data_info,.(Year, Lost_Deal),transform,Prop=(Value/sum(Value)))
+
+prop_data <- prop_data %>% mutate(lab.ypos = cumsum(Prop) - 0.5*Prop)
+
+# Rename type of lost_deal (True -> Lost, False -> Deal)
+levels(prop_data$Lost_Deal) <- c("Deal", "Lost_Deal", "Lost")
+
+plot_pie_state(prop_data)
 dev.off()

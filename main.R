39fab35f9448b675e4ff27783ed748bdf3741c8b
Maria Luiza <mlnr@cesar.org.br>
2019-08-07 00:26:02 -0300

    Analysis in clients through years

--- main.R
+++ main.R
@@ -7,6 +7,7 @@ source("analysis.R") # Functions to extract info on data
 source("plotter.R") # Functions to plotter data
 
 library(reshape2)
+require(dplyr)
 require(gridExtra)
 
 # 1. Load the data
@@ -69,30 +70,33 @@ names(data_gen) <- c("Deal", "Lost")
 
 # 4. Data Evaluation
 
-# # 4.1 Value per month per Year
-# 
-# plots <- list()
-# for(year in years){
-#     d1 <- aggregate(data_gen$Deal$Data[[year]]$Value ~ data_gen$Deal$Data[[year]]$Month,
-#                     data_gen$Deal$Data[[year]],
-#                     mean)
-#     d2 <- aggregate(data_gen$Lost$Data[[year]]$Value ~ data_gen$Lost$Data[[year]]$Month,
-#                     data_gen$Lost$Data[[year]],
-#                     mean)
-# 
-#     names(d2) <- c("Month", "Lost Value")
-#     d1 <- cbind(d1, d2$`Lost Value`)
-# 
-#     names(d1) <- c("Month", "Deal Value", "Lost Value")
-#     d1$Month <- with(d1, month.abb[Month])
-#     d1 <- d1[order(match(d1$Month, month.name)), ]
-# 
-#     dfm <- melt(d1[,c('Month','Deal Value','Lost Value')],id.vars = 1)
-#     plots[[year]] <- plot_month_value(dfm, 1, 2, 3, year)
-# }
-# do.call(grid.arrange, c(plots, nrow=length(years)))
+# 4.1 Value per month per Year
+png(filename = "value_month_year.png", width = 825, height = 660, units = "px")
 
-# 4.2 Executive sales thourgh years
+plots <- list()
+for(year in years){
+    d1 <- aggregate(data_gen$Deal$Data[[year]]$Value ~ data_gen$Deal$Data[[year]]$Month,
+                    data_gen$Deal$Data[[year]],
+                    mean)
+    d2 <- aggregate(data_gen$Lost$Data[[year]]$Value ~ data_gen$Lost$Data[[year]]$Month,
+                    data_gen$Lost$Data[[year]],
+                    mean)
+
+    names(d2) <- c("Month", "Lost Value")
+    d1 <- cbind(d1, d2$`Lost Value`)
+
+    names(d1) <- c("Month", "Deal Value", "Lost Value")
+    d1$Month <- with(d1, month.abb[Month])
+    d1 <- d1[order(match(d1$Month, month.name)), ]
+
+    dfm <- melt(d1[,c('Month','Deal Value','Lost Value')],id.vars = 1)
+    plots[[year]] <- plot_month_value(dfm, 1, 2, 3, year)
+}
+do.call(grid.arrange, c(plots, nrow=length(years)))
+dev.off()
+
+# 4.2 Sales executive thourgh years
+png(filename = "sales_executive_years.png", width = 770, height = 665, units = "px")
 
 plots <- list()
 for(year in years){
@@ -113,19 +117,31 @@ for(year in years){
 }
 
 do.call(grid.arrange, c(plots, nrow=length(years)))
+dev.off()
 
-# # 4.3 Get information about client who's paid more
-# clients <- list()
-# for(year in years){
-#     data <- data_gen$Deal
-#     mean_year <- mean(data$Data[[year]]$Value)
-#     sd_year <- sd(data$Data[[year]]$Value)
-#     
-#     client <- names(which.max(table(data$Data[[year]]$Client)))
-#     print(data$Data[[year]][data$Data[[year]]$Client == client, ])
-#     
-#     
-#     # print(data$MaxVal[[year]])
-#     # print(paste0("Mean: ", mean_year, ", Sd: ", sd_year))
-# }
-# # 5. Plotter data
+# 4.3 Get information about client who's paid more
+clients <- list()
+deals <- c("Deal", "Lost")
+for(year in years){
+    for(deal in deals){
+        data1 <- data_gen[[deal]]
+        # Get client most frequent through the year
+        client <- names(which.max(table(data1$Data[[year]]$Client)))
+
+        # Get information about him
+        client_info <- data1$Data[[year]][data1$Data[[year]]$Client == client, ]
+        mean_client <- mean(client_info$Value)
+        executives <- unique(client_info$Sales_Executive)
+
+        clients[[deal]][[year]] <- list(client, mean_client, executives)
+    }
+}
+
+print(clients)
+
+# 4.4 Summary Boxplot
+png(filename = "summary_years.png", width = 825, height = 660, units = "px")
+# Get only information used to evaluate
+box_data <- select(data, Year, Value, Lost_Deal)
+plot_boxplot_summary(box_data)
+dev.off()
